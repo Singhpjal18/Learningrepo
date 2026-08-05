1aa420b07468ef472588c8c7390f9ae0fd54a8de
autobotl <singhpjal18@gmail.com>
2026-08-05 21:42:37 +0530

    provide fixed

--- Environments/PREPROD/Provider.tf
+++ Environments/PREPROD/Provider.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       Source  = "hashicorp/azurerm"
-      version = "5.0.0"
+      version = "=5.0.0"
     }
   }
 }
-provider "Azurerm" {
+provider "azurerm" {
   features {}
 }
